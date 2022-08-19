a718520244eccc22852618a028b680363052dec9
wangjinsheng <1334258066@qq.com>
2022-08-19 17:42:19 +0800

    提交

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'lib'),
-        filename: 'index.js'
+        filename: 'index.js',
+        libraryTarget: 'umd' // 添加输出库配置
     }
 };
